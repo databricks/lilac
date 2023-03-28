15b674cd1f5774b9e5c6ce486ddf09a94b2a410c
Daniel Smilkov <dsmilkov@gmail.com>
2023-03-28 15:30:01 -0400

    save

--- src/concepts/db_concept_test.py
+++ src/concepts/db_concept_test.py
@@ -68,3 +68,70 @@ class DBConceptSuite:
 
     updated_example = concept.data[example.id]
     assert updated_example == Example(id=example.id, label=False, text='not in concept, updated')
+
+  def test_remove_concept(self, db_cls: Type[ConceptDB]) -> None:
+    db = db_cls()
+    namespace = 'test'
+    concept_name = 'test_concept'
+    train_data = [
+        ExampleIn(label=False, text='not in concept'),
+        ExampleIn(label=True, text='in concept')
+    ]
+    db.edit(namespace, concept_name, ConceptUpdate(insert=train_data))
+
+    db.remove(namespace, concept_name)
+
+    concept = db.get(namespace, concept_name)
+
+    assert concept is None
+
+  def test_remove_concept_examples(self, db_cls: Type[ConceptDB]) -> None:
+    db = db_cls()
+    namespace = 'test'
+    concept_name = 'test_concept'
+    train_data = [
+        ExampleIn(label=False, text='not in concept'),
+        ExampleIn(label=True, text='in concept')
+    ]
+    db.edit(namespace, concept_name, ConceptUpdate(insert=train_data))
+
+    concept = db.get(namespace, concept_name)
+
+    assert concept is not None
+
+    example = list(concept.data.values())[0]
+
+    db.edit(namespace, concept_name, ConceptUpdate(remove=[example.id]))
+
+    concept = db.get(namespace, concept_name)
+
+    assert concept is not None
+    assert len(concept.data.keys()) == 1
+    assert example.id not in concept.data.keys()
+
+  def test_remove_invalid_id(self, db_cls: Type[ConceptDB]) -> None:
+    db = db_cls()
+    namespace = 'test'
+    concept_name = 'test_concept'
+    train_data = [
+        ExampleIn(label=False, text='not in concept'),
+        ExampleIn(label=True, text='in concept')
+    ]
+    db.edit(namespace, concept_name, ConceptUpdate(insert=train_data))
+
+    with pytest.raises(ValueError, match='Example with id "invalid_id" does not exist'):
+      db.edit(namespace, concept_name, ConceptUpdate(remove=['invalid_id']))
+
+  def test_edit_invalid_id(self, db_cls: Type[ConceptDB]) -> None:
+    db = db_cls()
+    namespace = 'test'
+    concept_name = 'test_concept'
+    train_data = [
+        ExampleIn(label=False, text='not in concept'),
+        ExampleIn(label=True, text='in concept')
+    ]
+    db.edit(namespace, concept_name, ConceptUpdate(insert=train_data))
+
+    with pytest.raises(ValueError, match='Example with id "invalid_id" does not exist'):
+      db.edit(namespace, concept_name,
+              ConceptUpdate(update=[Example(id='invalid_id', label=False, text='not in concept')]))
