8e17662e049fb3dba15dab43afb2072d96947701
Daniel Smilkov <dsmilkov@gmail.com>
2023-04-05 13:48:56 -0400

    save

---
+++ src/web/fastapi_client/models/Comparison.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * The comparison operator between a column and a feature value.
+ */
+export enum Comparison {
+    _ = '=',
+}
---
+++ src/web/fastapi_client/models/GroupsSortBy.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * The sort for groups queries.
+ *
+ * Either "count" which sorts by the count of feature value, or "value" which sorts by the
+ * feature value itself.
+ */
+export enum GroupsSortBy {
+    COUNT = 'count',
+    VALUE = 'value',
+}
