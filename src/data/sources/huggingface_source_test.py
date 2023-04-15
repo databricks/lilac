62f6e6b6256b782b7db2f8a168c8faa7fab00024
nsthorat <nsthorat@gmail.com>
2023-04-15 17:58:22 -0400

    Support sequences from HuggingFace.

--- src/data/sources/huggingface_source_test.py
+++ src/data/sources/huggingface_source_test.py
@@ -2,10 +2,8 @@
 import os
 import pathlib
 
-import pandas as pd
-
 # mypy: disable-error-code="attr-defined"
-from datasets import Dataset
+from datasets import Dataset, Features, Sequence, Value
 
 from ...schema import UUID_COLUMN, DataType, Field, Schema
 from .huggingface_source import HF_SPLIT_COLUMN, HuggingFaceDataset
@@ -13,8 +11,7 @@ from .source import SourceProcessResult
 
 
 def test_simple_hf(tmp_path: pathlib.Path) -> None:
-  df = pd.DataFrame.from_records([{'x': 1, 'y': '10'}, {'x': 1, 'y': '10'}])
-  dataset = Dataset.from_pandas(df)
+  dataset = Dataset.from_list([{'x': 1, 'y': '10'}, {'x': 1, 'y': '10'}])
 
   dataset_name = os.path.join(tmp_path, 'hf-test-dataset')
   dataset.save_to_disk(dataset_name)
@@ -29,6 +26,42 @@ def test_simple_hf(tmp_path: pathlib.Path) -> None:
           HF_SPLIT_COLUMN: Field(dtype=DataType.STRING),
           'x': Field(dtype=DataType.INT64),
           'y': Field(dtype=DataType.STRING),
+      }),
+                                        num_items=2,
+                                        filepaths=[])
+
+  # Validate except for the filepaths, which are not deterministic.
+  expected_result.filepaths = result.filepaths
+  assert result == expected_result
+  assert len(result.filepaths) == 1
+
+
+def test_simple_hf_sequence(tmp_path: pathlib.Path) -> None:
+  dataset = Dataset.from_list([{
+      'x': 1,
+      'y': [1, 0]
+  }, {
+      'x': 2,
+      'y': [2, 0]
+  }],
+                              features=Features({
+                                  'x': Value(dtype='int64'),
+                                  'y': Sequence(feature=Value(dtype='int64'))
+                              }))
+
+  dataset_name = os.path.join(tmp_path, 'hf-test-dataset')
+  dataset.save_to_disk(dataset_name)
+
+  source = HuggingFaceDataset(dataset_name=dataset_name, load_from_disk=True)
+
+  result = source.process(str(os.path.join(tmp_path, 'data')))
+
+  expected_result = SourceProcessResult(data_schema=Schema(
+      fields={
+          UUID_COLUMN: Field(dtype=DataType.BINARY),
+          HF_SPLIT_COLUMN: Field(dtype=DataType.STRING),
+          'x': Field(dtype=DataType.INT64),
+          'y': Field(repeated_field=Field(dtype=DataType.INT64)),
       }),
                                         num_items=2,
                                         filepaths=[])
