f99a03ed1fd58b2a050d4b78d1b00e9df1bf1555
nsthorat <nsthorat@gmail.com>
2023-04-16 20:13:45 -0400

    save

--- src/data/sources/huggingface_source_test.py
+++ src/data/sources/huggingface_source_test.py
@@ -6,19 +6,20 @@ import pathlib
 from datasets import Dataset, Features, Sequence, Value
 
 from ...schema import UUID_COLUMN, DataType, Field, Schema
+from ...test_utils import read_items
 from .huggingface_source import HF_SPLIT_COLUMN, HuggingFaceDataset
 from .source import SourceProcessResult
 
 
-def test_simple_hf(tmp_path: pathlib.Path) -> None:
-  dataset = Dataset.from_list([{'x': 1, 'y': '10'}, {'x': 1, 'y': '10'}])
+def test_hf(tmp_path: pathlib.Path) -> None:
+  dataset = Dataset.from_list([{'x': 1, 'y': '10'}, {'x': 2, 'y': '20'}])
 
   dataset_name = os.path.join(tmp_path, 'hf-test-dataset')
   dataset.save_to_disk(dataset_name)
 
   source = HuggingFaceDataset(dataset_name=dataset_name, load_from_disk=True)
 
-  result = source.process(str(os.path.join(tmp_path, 'data')))
+  result = source.process(str(tmp_path))
 
   expected_result = SourceProcessResult(data_schema=Schema(
       fields={
@@ -30,43 +31,90 @@ def test_simple_hf(tmp_path: pathlib.Path) -> None:
                                         num_items=2,
                                         filepaths=[])
 
-  # Validate except for the filepaths, which are not deterministic.
-  expected_result.filepaths = result.filepaths
-  assert result == expected_result
-  assert len(result.filepaths) == 1
-
-
-def test_simple_hf_sequence(tmp_path: pathlib.Path) -> None:
-  dataset = Dataset.from_list([{
+  items = read_items(tmp_path, result.filepaths, expected_result.data_schema)
+  assert items == [{
+      UUID_COLUMN: items[0][UUID_COLUMN],
+      HF_SPLIT_COLUMN: 'default',
       'x': 1,
-      'y': [1, 0]
+      'y': '10',
   }, {
+      UUID_COLUMN: items[1][UUID_COLUMN],
+      HF_SPLIT_COLUMN: 'default',
       'x': 2,
-      'y': [2, 0]
-  }],
-                              features=Features({
-                                  'x': Value(dtype='int64'),
-                                  'y': Sequence(feature=Value(dtype='int64'))
-                              }))
+      'y': '20',
+  }]
+
+
+def test_hf_sequence(tmp_path: pathlib.Path) -> None:
+  dataset = Dataset.from_list(
+      [{
+          'scalar': 1,
+          'seq': [1, 0],
+          'seq_dict': {
+              'x': [1, 2, 3],
+              'y': ['four', 'five', 'six']
+          }
+      }, {
+          'scalar': 2,
+          'seq': [2, 0],
+          'seq_dict': {
+              'x': [10, 20, 30],
+              'y': ['forty', 'fifty', 'sixty']
+          }
+      }],
+      features=Features({
+          'scalar': Value(dtype='int64'),
+          'seq': Sequence(feature=Value(dtype='int64')),
+          'seq_dict': Sequence(feature={
+              'x': Value(dtype='int64'),
+              'y': Value(dtype='string')
+          })
+      }))
 
   dataset_name = os.path.join(tmp_path, 'hf-test-dataset')
   dataset.save_to_disk(dataset_name)
 
   source = HuggingFaceDataset(dataset_name=dataset_name, load_from_disk=True)
 
-  result = source.process(str(os.path.join(tmp_path, 'data')))
+  result = source.process(str(tmp_path))
 
   expected_result = SourceProcessResult(data_schema=Schema(
       fields={
-          UUID_COLUMN: Field(dtype=DataType.BINARY),
-          HF_SPLIT_COLUMN: Field(dtype=DataType.STRING),
-          'x': Field(dtype=DataType.INT64),
-          'y': Field(repeated_field=Field(dtype=DataType.INT64)),
+          UUID_COLUMN:
+              Field(dtype=DataType.BINARY),
+          HF_SPLIT_COLUMN:
+              Field(dtype=DataType.STRING),
+          'scalar':
+              Field(dtype=DataType.INT64),
+          'seq':
+              Field(repeated_field=Field(dtype=DataType.INT64)),
+          'seq_dict':
+              Field(
+                  fields={
+                      'x': Field(repeated_field=Field(dtype=DataType.INT64)),
+                      'y': Field(repeated_field=Field(dtype=DataType.STRING)),
+                  }),
       }),
                                         num_items=2,
                                         filepaths=[])
 
-  # Validate except for the filepaths, which are not deterministic.
-  expected_result.filepaths = result.filepaths
-  assert result == expected_result
-  assert len(result.filepaths) == 1
+  items = read_items(tmp_path, result.filepaths, expected_result.data_schema)
+  assert items == [{
+      UUID_COLUMN: items[0][UUID_COLUMN],
+      HF_SPLIT_COLUMN: 'default',
+      'scalar': 1,
+      'seq': [1, 0],
+      'seq_dict': {
+          'x': [1, 2, 3],
+          'y': ['four', 'five', 'six']
+      }
+  }, {
+      UUID_COLUMN: items[1][UUID_COLUMN],
+      HF_SPLIT_COLUMN: 'default',
+      'scalar': 2,
+      'seq': [2, 0],
+      'seq_dict': {
+          'x': [10, 20, 30],
+          'y': ['forty', 'fifty', 'sixty']
+      }
+  }]
