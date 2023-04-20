05cb01ba0bf187e4bf89db7ea6619c14a9fdd207
nsthorat <nsthorat@gmail.com>
2023-04-19 21:22:32 -0400

    save

--- src/embeddings/vector_store_test.py
+++ src/embeddings/vector_store_test.py
@@ -18,14 +18,13 @@ class VectorStoreSuite:
   def test_get_all(self, tmp_path: pathlib.Path, store_cls: Type[VectorStore]) -> None:
     store = store_cls()
 
-    store.add(['a', 'b', 'c'], np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    store.add(['a', 'b', 'c'], np.array([[1, 2], [3, 4], [5, 6]]))
 
-    np.testing.assert_array_equal(store.get(['a', 'b', 'c']),
-                                  np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    np.testing.assert_array_equal(store.get(['a', 'b', 'c']), np.array([[1, 2], [3, 4], [5, 6]]))
 
   def test_get_subset(self, tmp_path: pathlib.Path, store_cls: Type[VectorStore]) -> None:
     store = store_cls()
 
-    store.add(['a', 'b', 'c'], np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    store.add(['a', 'b', 'c'], np.array([[1, 2], [3, 4], [5, 6]]))
 
-    np.testing.assert_array_equal(store.get(['b', 'c']), np.array([[4, 5, 6], [7, 8, 9]]))
+    np.testing.assert_array_equal(store.get(['b', 'c']), np.array([[3, 4], [5, 6]]))
