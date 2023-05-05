96700dff72ddad831e59e326ef3023c9b4ddeb74
Jonas Jongejan <jonas@halfdanj.dk>
2023-05-05 18:24:43 -0400

    Client lib for deserializing api response

---
+++ src/webClientLib/lilac.spec.ts
@@ -0,0 +1,242 @@
+import { describe, expect, it } from 'vitest';
+import { Schema } from './fastapi_client';
+import { deserializeRow, deserializeSchema, getField, listFields, listValues } from './lilac';
+import { ENTITY_FEATURE_KEY } from './schema';
+
+const MANIFEST_SCHEMA_FIXTURE: Schema = {
+  fields: {
+    title: {
+      dtype: 'string'
+    },
+    comment_text: {
+      dtype: 'string'
+    },
+    complex_field: {
+      dtype: 'struct',
+      fields: {
+        propertyA: {
+          dtype: 'string'
+        },
+        propertyB: {
+          dtype: 'string'
+        }
+      }
+    },
+    tags: {
+      dtype: 'list',
+      repeated_field: {
+        dtype: 'string'
+      }
+    },
+    complex_list_of_struct: {
+      dtype: 'list',
+      repeated_field: {
+        dtype: 'struct',
+        fields: {
+          propertyA: {
+            dtype: 'string'
+          },
+          propertyB: {
+            dtype: 'string'
+          }
+        }
+      }
+    },
+    __rowid__: {
+      dtype: 'string'
+    },
+    __lilac__: {
+      fields: {
+        comment_text: {
+          fields: {
+            pii: {
+              fields: {
+                emails: {
+                  repeated_field: {
+                    fields: {},
+                    dtype: 'string_span',
+                    is_entity: true,
+                    derived_from: ['comment_text']
+                  },
+                  dtype: 'list',
+                  derived_from: ['comment_text']
+                }
+              },
+              dtype: 'struct',
+              signal_root: true,
+              derived_from: ['comment_text']
+            }
+          },
+          dtype: 'struct'
+        },
+        complex_field: {
+          dtype: 'struct',
+          fields: {
+            propertyA: {
+              dtype: 'struct',
+              fields: {
+                text_statistics: {
+                  fields: {
+                    num_characters: {
+                      dtype: 'int32',
+                      derived_from: ['review']
+                    }
+                  },
+                  dtype: 'struct',
+                  signal_root: true,
+                  derived_from: ['review']
+                }
+              }
+            }
+          }
+        }
+      },
+      dtype: 'struct'
+    }
+  }
+};
+
+const SELECT_ROWS_RESPONSE_FIXTURE = {
+  title: 'title text',
+  comment_text: 'text content',
+  tags: ['tag1', 'tag2'],
+  complex_field: {
+    propertyA: 'valueA',
+    propertyB: 'valueB'
+  },
+  complex_list_of_struct: [
+    {
+      propertyA: 'valueA',
+      propertyB: 'valueB'
+    },
+    {
+      propertyA: 'valueC',
+      propertyB: 'valueD'
+    }
+  ],
+  __rowid__: 'hNRA5Z_GKkHNiqn0',
+  __lilac__: {
+    comment_text: {
+      pii: {
+        emails: [
+          {
+            [ENTITY_FEATURE_KEY]: {
+              start: 1,
+              end: 19
+            }
+          },
+          {
+            [ENTITY_FEATURE_KEY]: {
+              start: 82,
+              end: 100
+            }
+          }
+        ]
+      }
+    },
+    complex_field: {
+      propertyA: {
+        text_statistics: {
+          num_characters: 100
+        }
+      }
+    }
+  }
+};
+
+describe('lilac', () => {
+  const schema = deserializeSchema(MANIFEST_SCHEMA_FIXTURE);
+  const row = deserializeRow(SELECT_ROWS_RESPONSE_FIXTURE, schema);
+
+  describe('deserializeSchema', () => {
+    it('should deserialize a schema', () => {
+      expect(schema).toBeDefined();
+      expect(schema.fields?.title).toBeDefined();
+      expect(schema.fields?.__lilac__).toBeUndefined();
+    });
+
+    it('merges signals into the source fields', () => {
+      expect(schema.fields?.comment_text).toBeDefined();
+      expect(schema.fields?.comment_text.fields?.pii).toBeDefined();
+    });
+
+    it('adds paths to all fields', () => {
+      expect(schema.fields?.title.path).toEqual(['title']);
+      expect(schema.fields?.complex_field.fields?.propertyA.path).toEqual([
+        'complex_field',
+        'propertyA'
+      ]);
+
+      expect(schema.fields?.complex_list_of_struct.repeated_field?.fields?.propertyA.path).toEqual([
+        'complex_list_of_struct',
+        '*',
+        'propertyA'
+      ]);
+
+      expect(schema.fields?.comment_text.fields?.pii.path).toEqual([
+        '__lilac__',
+        'comment_text',
+        'pii'
+      ]);
+    });
+  });
+
+  describe('deserializeRow', () => {
+    it('should deserialize a row', () => {
+      expect(row).toBeDefined();
+
+      expect((row as any).children.title).toBeDefined();
+      expect((row as any).children?.__lilac__).toBeUndefined();
+      expect((row as any).children.title.value).toEqual('title text');
+      expect((row as any).children.complex_field.children.propertyA.value).toEqual('valueA');
+      expect(
+        (row as any).children.complex_list_of_struct.children[0].children.propertyA.value
+      ).toEqual('valueA');
+    });
+
+    it('merges signals into the source fields', () => {
+      expect(
+        (row as any).children.comment_text.children.pii.children.emails.children[0].value
+      ).toEqual({
+        end: 19,
+        start: 1
+      });
+    });
+  });
+
+  describe('listFields', () => {
+    it('should return a list of fields', () => {
+      const fields = listFields(schema);
+      expect(fields).toBeDefined();
+      expect(fields[0].dtype).toEqual('string');
+      const paths = fields.map((f) => f.path);
+      expect(paths).toContainEqual(['title']);
+      expect(paths).toContainEqual(['complex_list_of_struct', '*']);
+      expect(paths).toContainEqual(['complex_list_of_struct', '*', 'propertyA']);
+    });
+  });
+
+  describe('listValues', () => {
+    it('should return a list of values', () => {
+      const values = listValues(row);
+      expect(values).toBeDefined();
+      expect(values[0].path).toEqual(['title']);
+      expect(values[0].value).toEqual('title text');
+      const paths = values.map((f) => f.path);
+      expect(paths).toContainEqual(['title']);
+      expect(paths).toContainEqual(['complex_list_of_struct', '*']);
+      expect(paths).toContainEqual(['complex_list_of_struct', '*', 'propertyA']);
+    });
+  });
+
+  describe('getField', () => {
+    it('should return simple paths', () => {
+      const field = getField(schema, ['title']);
+      expect(field?.path).toEqual(['title']);
+    });
+    it('should return a field by path with repeated fields', () => {
+      const field = getField(schema, ['complex_list_of_struct', '*', 'propertyA']);
+      expect(field?.path).toEqual(['complex_list_of_struct', '*', 'propertyA']);
+    });
+  });
+});
---
+++ src/webClientLib/lilac.ts
@@ -0,0 +1,145 @@
+import { DataType, Field, Schema } from './fastapi_client';
+import { castDataType, ENTITY_FEATURE_KEY, LILAC_COLUMN, Path, PATH_WILDCARD } from './schema';
+import { mergeDeep } from './utils';
+
+export type LilacSchemaField = Field & {
+  path: Path;
+  // Overwrite the fields and repeated_field properties to be LilacSchemaField
+  repeated_field?: LilacSchemaField;
+  fields?: Record<string, LilacSchemaField>;
+};
+export type LilacSchema = Omit<LilacSchemaField, 'path'>;
+// {
+//   fields: Record<string, LilacSchemaField>;
+// };
+
+export type LilacItemNode<D extends DataType = DataType> = {
+  value: castDataType<D>;
+  children?: LilacItemNode[] | Record<string, LilacItemNode>;
+  path: Path;
+};
+
+/**
+ * Deserialize a raw schema response to a LilacSchema.
+ */
+export function deserializeSchema(rawSchema: Schema): LilacSchema {
+  const lilacFields = lilacSchemaFieldFromField(rawSchema, []);
+
+  if (!lilacFields.fields) {
+    return { fields: {} };
+  }
+
+  let { [LILAC_COLUMN]: signalsFields, ...fields } = lilacFields.fields;
+
+  // Merge the signal fields into the source fields
+  if (signalsFields?.fields) {
+    fields = mergeDeep(fields, signalsFields.fields);
+  }
+
+  // Convert the fields to LilacSchemaField
+  return { fields };
+}
+
+export function deserializeRow(rawRow: object, schema: LilacSchema): LilacItemNode {
+  // const fields = listFields(schema);
+
+  const { children } = lilacItemNodeFromRawValues(rawRow, []);
+
+  if (Array.isArray(children)) {
+    throw new Error('Expected row to have a single root node');
+  }
+  if (!children) {
+    throw new Error('Expected row to have children');
+  }
+
+  const { [LILAC_COLUMN]: signalValues, ...values } = children;
+
+  let mergedChildren: LilacItemNode['children'] = values;
+  if (signalValues) mergedChildren = mergeDeep(values, signalValues.children);
+
+  return {
+    value: null,
+    children: mergedChildren,
+    path: []
+  };
+}
+
+/** List all fields as a flattend array */
+export function listFields(schema: LilacSchemaField | LilacSchema): LilacSchemaField[] {
+  return [
+    ...Object.values(schema.fields || {}),
+    ...Object.values(schema.fields || {}).flatMap(listFields),
+    ...(schema.repeated_field ? [schema.repeated_field] : []),
+    ...(schema.repeated_field ? listFields(schema.repeated_field) : [])
+  ];
+}
+
+/** List all values as a flattend array */
+export function listValues(row: LilacItemNode): LilacItemNode[] {
+  return [
+    ...Object.values(row.children || {}),
+    ...Object.values(row.children || {}).flatMap(listValues)
+  ];
+}
+
+/**
+ * Get a field in schema by path
+ */
+export function getField(schema: LilacSchema, path: Path): LilacSchemaField | undefined {
+  const list = listFields(schema);
+  return list.find((field) => field.path.join('.') === path.join('.'));
+}
+
+/**
+ * Convert raw schema field to LilacSchemaField.
+ * Adds path attribute to each field
+ */
+function lilacSchemaFieldFromField(field: Field, path: Path): LilacSchemaField {
+  const { fields, repeated_field, ...rest } = field;
+  const lilacField: LilacSchemaField = { ...rest, path: [] };
+  if (fields) {
+    lilacField.fields = {};
+    for (const [fieldName, field] of Object.entries(fields)) {
+      const lilacChildField = lilacSchemaFieldFromField(field, [...path, fieldName]);
+      lilacChildField.path = [...path, fieldName];
+      lilacField.fields[fieldName] = lilacChildField;
+    }
+  }
+  if (repeated_field) {
+    const lilacChildField = lilacSchemaFieldFromField(repeated_field, [...path, PATH_WILDCARD]);
+    lilacChildField.path = [...path, PATH_WILDCARD];
+    lilacField.repeated_field = lilacChildField;
+  }
+  return lilacField;
+}
+
+function lilacItemNodeFromRawValues(
+  rawValue: any,
+  // fields: LilacSchemaField[],
+  path: Path
+): LilacItemNode {
+  if (Array.isArray(rawValue)) {
+    return {
+      value: null,
+      path,
+      children: rawValue.map((value, index) =>
+        lilacItemNodeFromRawValues(value, [...path, PATH_WILDCARD])
+      )
+    };
+  } else if (typeof rawValue === 'object') {
+    const { [ENTITY_FEATURE_KEY]: entityValue, ...rest } = rawValue;
+    return {
+      value: entityValue || null,
+      path,
+      children: Object.entries(rest).reduce<Record<string, LilacItemNode>>((acc, [key, value]) => {
+        acc[key] = lilacItemNodeFromRawValues(value, [...path, key]);
+        return acc;
+      }, {})
+    };
+  } else {
+    return {
+      value: rawValue,
+      path
+    };
+  }
+}
