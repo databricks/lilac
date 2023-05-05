22bf24d4bd63a7750c1ab1b8197247b9f17f4f9c
nsthorat <nsthorat@gmail.com>
2023-05-05 18:06:15 -0400

    save

--- src/embeddings/cohere.py
+++ src/embeddings/cohere.py
@@ -33,9 +33,9 @@ class Cohere(Embedding):
     """Call the embedding function."""
     batches = chunks(data, COHERE_BATCH_SIZE)
     for batch in batches:
-      lbatch = list(batch)
-      print(lbatch)
+      # Cohere errors out when there's non-string data. For now, we cast to a string.
+      batch = (str(val) for val in batch)
       embedding_batch = normalize(np.array(_cohere().embed(
-          lbatch, truncate='END').embeddings)).astype(np.float16)
+          batch, truncate='END').embeddings)).astype(np.float16)
       # np.split returns a shallow copy of each embedding so we don't increase the memory footprint.
       yield from (EmbeddingEntity(e) for e in np.split(embedding_batch, COHERE_BATCH_SIZE))
