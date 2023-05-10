aa5081ff1636a23fd3dae51d77f0b5448011227e
Daniel Smilkov <dsmilkov@gmail.com>
2023-05-10 14:29:53 -0400

    save

---
+++ scripts/fix_py.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Fail if any of the commands below fail.
+set -e
+
+# Activate the current py virtual env.
+source $(poetry env info --path)/bin/activate
+
+echo "Fixing python with ruff..."
+ruff --fix src/
+
+echo "Fixing python imports with isort..."
+isort src/
