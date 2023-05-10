603acd855c9ac516da16b08b5178941a9e5d6dde
Daniel Smilkov <dsmilkov@gmail.com>
2023-05-10 17:07:28 -0400

    save

--- src/data/db_dataset_select_rows_schema_test.py
+++ src/data/db_dataset_select_rows_schema_test.py
@@ -3,22 +3,15 @@
 import pathlib
 from typing import Generator, Iterable, Optional, Type, cast
 
-import numpy as np
 import pytest
-from typing_extensions import override
 
 from ..config import CONFIG
-from ..embeddings.embedding import EmbeddingSignal
-from ..embeddings.vector_store import VectorStore
 from ..schema import (
   LILAC_COLUMN,
   UUID_COLUMN,
-  DataType,
   EnrichmentType,
   Field,
   Item,
-  ItemValue,
-  PathTuple,
   RichData,
   SignalOut,
   field,
@@ -27,7 +20,6 @@ from ..schema import (
 )
 from ..signals.signal import Signal
 from ..signals.signal_registry import clear_signal_registry, register_signal
-from .dataset_utils import signal_item
 from .db_dataset import DatasetDB, SignalUDF
 from .db_dataset_duckdb import DatasetDuckDB
 from .db_dataset_test_utils import make_db
@@ -74,50 +66,6 @@ TEST_DATA: list[Item] = [{
   }]
 }]
 
-EMBEDDINGS: list[tuple[str, list[float]]] = [
-  ('A', [1.0, 0.0, 0.0]),
-  ('B', [1.0, 1.0, 0.0]),
-  ('C', [1.0, 1.0, 1.0]),
-]
-
-STR_EMBEDDINGS: dict[str, list[float]] = {text: embedding for text, embedding in EMBEDDINGS}
-
-
-class TestEmbedding(EmbeddingSignal):
-  """A test embed function."""
-  name = 'test_embedding'
-  enrichment_type = EnrichmentType.TEXT
-
-  @override
-  def fields(self) -> Field:
-    """Return the fields for the embedding."""
-    # Override in the test so we can attach extra metadata.
-    return Field(dtype=DataType.EMBEDDING)
-
-  @override
-  def compute(self, data: Iterable[RichData]) -> Iterable[Item]:
-    """Call the embedding function."""
-    embeddings = [np.array(STR_EMBEDDINGS[cast(str, example)]) for example in data]
-    yield from (signal_item(e, metadata={'neg_sum': -1 * e.sum()}) for e in embeddings)
-
-
-class TestEmbeddingSumSignal(Signal):
-  """Sums the embeddings to return a single floating point value."""
-  name = 'test_embedding_sum'
-  enrichment_type = EnrichmentType.TEXT_EMBEDDING
-
-  @override
-  def fields(self) -> Field:
-    return field('float32')
-
-  @override
-  def vector_compute(self, keys: Iterable[PathTuple],
-                     vector_store: VectorStore) -> Iterable[ItemValue]:
-    # The signal just sums the values of the embedding.
-    embedding_sums = vector_store.get(keys).sum(axis=1)
-    for embedding_sum in embedding_sums.tolist():
-      yield embedding_sum
-
 
 @pytest.fixture(autouse=True)
 def set_data_path(tmp_path: pathlib.Path) -> Generator:
@@ -130,8 +78,8 @@ def set_data_path(tmp_path: pathlib.Path) -> Generator:
 @pytest.fixture(scope='module', autouse=True)
 def setup_teardown() -> Iterable[None]:
   # Setup.
-  register_signal(TestEmbedding)
-  register_signal(TestEmbeddingSumSignal)
+  register_signal(LengthSignal)
+  register_signal(AddSpaceSignal)
 
   # Unit test runs.
   yield
@@ -150,6 +98,18 @@ class LengthSignal(Signal):
   def compute(self, data: Iterable[RichData]) -> Iterable[Optional[SignalOut]]:
     for text_content in data:
       yield len(text_content)
+
+
+class AddSpaceSignal(Signal):
+  name = 'add_space_signal'
+  enrichment_type = EnrichmentType.TEXT
+
+  def fields(self) -> Field:
+    return field('string')
+
+  def compute(self, data: Iterable[RichData]) -> Iterable[Optional[SignalOut]]:
+    for text_content in data:
+      yield cast(str, text_content) + ' '
 
 
 @pytest.mark.parametrize('db_cls', ALL_DBS)
@@ -240,25 +200,23 @@ class SelectRowsSchemaSuite:
                                            db_cls: Type[DatasetDB]) -> None:
     db = make_db(db_cls, tmp_path, TEST_DATA)
 
-    db.compute_signal(TestEmbedding(), ('people', '*', 'name'))
+    db.compute_signal(AddSpaceSignal(), ('people', '*', 'name'))
     result = db.select_rows_schema(
-      [('people', '*', 'locations', '*', 'city'),
-       SignalUDF(TestEmbeddingSumSignal(),
-                 (LILAC_COLUMN, 'people', '*', 'name', 'test_embedding'))],
+      [('people', '*', 'name'), (LILAC_COLUMN,),
+       SignalUDF(AddSpaceSignal(), (LILAC_COLUMN, 'people', '*', 'name', 'add_space_signal'))],
       combine_columns=True)
     assert result == schema({
       UUID_COLUMN: 'string',
       'people': [{
-        'locations': [{
-          'city': 'string'
-        }]
+        'name': 'string'
       }],
       LILAC_COLUMN: {
         'people': [{
           'name': {
-            'test_embedding': {
-              'test_embedding_sum': signal_field(dtype='float32')
-            }
+            'add_space_signal': signal_field({
+              'add_space_signal': signal_field(dtype='string'),
+            },
+                                             dtype='string')
           }
         }]
       }
