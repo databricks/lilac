31c11a5c3d619c7bb9e2f0107ebbcc0493dc9fec
Jonas Jongejan <jonas@halfdanj.dk>
2023-05-11 15:14:18 -0400

    yaml to json

--- src/webSvelte/src/lib/components/queryBuilder/QueryBuilder.svelte
+++ src/webSvelte/src/lib/components/queryBuilder/QueryBuilder.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import {useGetSchemaQuery} from '$lib/store/apiDataset';
   import {getDatasetViewContext, getSelectRowsOptions} from '$lib/store/datasetViewStore';
-  import yaml from 'js-yaml';
 
   const datasetViewStore = getDatasetViewContext();
   $: schema = useGetSchemaQuery($datasetViewStore.namespace, $datasetViewStore.datasetName);
-
   $: options = $schema.data ? getSelectRowsOptions($datasetViewStore, $schema.data) : undefined;
-  $: optionsYaml = yaml.dump(options);
+  $: formattedOptions = options ? JSON.stringify(options, null, 2) : '';
 </script>
 
-<div class="whitespace-pre bg-gray-50 p-4 font-mono">
-  {optionsYaml}
-</div>
+<pre class="whitespace-pre bg-gray-50 p-4 font-mono">
+{formattedOptions}
+</pre>
