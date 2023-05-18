6cf2940c29769a9eb3d55cefec8cb4cf2098f149
nsthorat <nsthorat@gmail.com>
2023-05-18 11:34:23 -0400

    save

--- src/signals/signal.py
+++ src/signals/signal.py
@@ -204,8 +204,9 @@ class TextEmbeddingSignal(TextSignal):
   input_type = SignalInputType.TEXT
   compute_type = SignalInputType.TEXT
 
+  @override
   def fields(self) -> Field:
-    """Return the fields for the embedding."""
+    """NOTE: Override this method at your own risk. Embeddings should come with extra metadata."""
     return field('embedding')
 
 
