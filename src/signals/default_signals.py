a20e94440c019affca39c1266738b769ecd6d48e
nsthorat <nsthorat@gmail.com>
2023-05-22 11:32:05 -0400

    SemanticSearch => SemanticSimilarity

--- src/signals/default_signals.py
+++ src/signals/default_signals.py
@@ -3,7 +3,7 @@ from ..embeddings.cohere import Cohere
 from ..embeddings.sbert import SBERT
 from .concept_scorer import ConceptScoreSignal
 from .pii import PIISignal
-from .semantic_search import SemanticSearchSignal
+from .semantic_similarity import SemanticSimilaritySignal
 from .signal import register_signal
 from .splitters.text_splitter_spacy import SentenceSplitterSpacy
 from .text_statistics import TextStatisticsSignal
@@ -24,4 +24,4 @@ def register_default_signals() -> None:
   register_signal(SBERT)
 
   # Text embedding inputs.
-  register_signal(SemanticSearchSignal)
+  register_signal(SemanticSimilaritySignal)
