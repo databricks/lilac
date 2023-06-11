363d0d929d02f95486746c5a72d2b2ae08cda3a6
Daniel Smilkov <dsmilkov@gmail.com>
2023-06-11 11:58:42 -0400

    save

--- web/blueprint/src/lib/components/datasetView/RowItemValue.svelte
+++ web/blueprint/src/lib/components/datasetView/RowItemValue.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import {getDatasetContext} from '$lib/stores/datasetStore';
   /**
    * Component that renders a single value from a row in the dataset row view
    * In the case of strings with string_spans, it will render the derived string spans as well
@@ -8,13 +7,11 @@
   import {notEmpty} from '$lib/utils';
   import {
     L,
+    childFields,
     formatValue,
     getFieldsByDtype,
     getValueNodes,
-    isOrdinal,
-    listFieldParents,
-    pathIncludes,
-    type LilacSchema,
+    type LilacField,
     type LilacValueNode,
     type Path
   } from '$lilac';
@@ -22,50 +19,36 @@
 
   export let path: Path;
   export let row: LilacValueNode;
-  export let schema: LilacSchema;
+  export let field: LilacField;
 
-  let datasetStore = getDatasetContext();
-
-  $: visibleFields = $datasetStore?.visibleFields || [];
+  $: children = childFields(field);
 
   // Find the keyword span paths under this field.
-  $: visibleKeywordSpanFields = visibleFields
+  $: visibleKeywordSpanFields = children
     .filter(f => f.signal?.signal_name === 'substring_search')
     .flatMap(f => getFieldsByDtype('string_span', f));
 
   // Find the non-keyword span fields under this field.
-  $: visibleSpanFields = visibleFields
+  $: visibleSpanFields = children
     .filter(f => f.signal?.signal_name !== 'substring_search')
-    .filter(f => f.dtype === 'string_span')
-    .filter(f => pathIncludes(f.path, path));
+    .filter(f => f.dtype === 'string_span');
 
-  $: valueNodes = getValueNodes(row, path);
-  $: field = L.field(valueNodes[0]);
-
-  $: parents = field ? listFieldParents(field, schema) : undefined;
-  $: dtype = valueNodes.length && L.dtype(valueNodes[0]);
-  $: showValue =
-    valueNodes.length && // Hide if there are no values
-    dtype && // Hide if dtype is not set
-    (isOrdinal(dtype) || dtype == 'string') && // Hide if dtype is not ordinal or string
-    !parents?.some(parent => parent.dtype === 'string_span'); // Hide if any parent is a string span
-
-  $: values = valueNodes.map(v => L.value(v)).filter(notEmpty);
+  $: values = getValueNodes(row, path)
+    .map(v => L.value(v))
+    .filter(notEmpty);
 </script>
 
-{#if showValue && field}
-  <div class="flex flex-col">
-    <div class="font-mono text-sm text-gray-600">
-      {path.join('.')}
-    </div>
-
-    <div>
-      <StringSpanHighlight
-        text={formatValue(values[0])}
-        {row}
-        {visibleKeywordSpanFields}
-        {visibleSpanFields}
-      />
-    </div>
+<div class="flex flex-col">
+  <div class="font-mono text-sm text-gray-600">
+    {path.join('.')}
   </div>
-{/if}
+
+  <div>
+    <StringSpanHighlight
+      text={formatValue(values[0])}
+      {row}
+      {visibleKeywordSpanFields}
+      {visibleSpanFields}
+    />
+  </div>
+</div>
