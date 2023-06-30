b6661cfb49d505f516d8f3639592e942b1dc2773
Daniel Smilkov <dsmilkov@gmail.com>
2023-06-30 13:54:31 -0400

    save

--- web/blueprint/src/lib/components/schemaView/Histogram.svelte
+++ web/blueprint/src/lib/components/schemaView/Histogram.svelte
@@ -1,39 +1,46 @@
 <script lang="ts">
-  import {formatValue, type LeafValue, type LilacField} from '$lilac';
+  import type {LeafValue, LilacField} from '$lilac';
   import {createEventDispatcher} from 'svelte';
 
   export let field: LilacField;
   export let counts: Array<[LeafValue, number]>;
-  export let bins: Array<[string, number | null, number | null]> | null;
-
+  export let bins: Record<string, [number | null, number | null]> | null;
   $: maxCount = Math.max(...counts.map(([_, count]) => count));
 
-  function formatBin(bin: [string, number | null, number | null]): string {
-    const [binName, start, end] = bin;
-    if (field.bins != null) {
-      return binName;
+  function formatValue(value: LeafValue): string {
+    if (value == null) {
+      return 'N/A';
+    }
+    // If there are no bins, or there are named bins, we can just format the value.
+    if (bins == null || field.bins != null) {
+      return value.toString();
     }
     // If the field didn't have named bins, we need to format the start and end values.
+    const [start, end] = bins[value.toString()];
     if (start == null) {
-      return `< ${formatValue(end)}`;
+      return `< ${formatNumber(end!)}`;
     } else if (end == null) {
-      return `≥ ${formatValue(start)}`;
+      return `≥ ${formatNumber(start)}`;
     } else {
-      return `${formatValue(start)} .. ${formatValue(end)}`;
+      return `${formatNumber(start)} .. ${formatNumber(end)}`;
     }
+  }
+
+  function formatNumber(count: number): string {
+    return count.toLocaleString();
   }
   const dispatch = createEventDispatcher();
 </script>
 
 <div class="histogram">
-  {#each counts as [value, count], i}
-    {@const groupName = bins != null ? formatBin(bins[i]) : value?.toString()}
+  {#each counts as [value, count]}
+    {@const groupName = formatValue(value)}
     {@const barWidth = `${(count / maxCount) * 100}%`}
-    {@const formattedCount = formatValue(count)}
+    {@const formattedCount = formatNumber(count)}
 
     <button
       class="flex items-center text-left text-xs text-black hover:bg-gray-200"
-      on:click={() => dispatch('row-click', {value, index: i})}
+      on:click={() => dispatch('row-click', {value})}
     >
       <div title={groupName} class="w-48 flex-none truncate px-2">{groupName}</div>
       <div class="w-36 border-l border-gray-300 pl-2">
