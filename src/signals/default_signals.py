fbaba714ee6855d9dfd455c00536a696df1f5971
Daniel Smilkov <dsmilkov@gmail.com>
2023-07-06 14:20:05 -0400

    save

--- src/signals/default_signals.py
+++ src/signals/default_signals.py
@@ -1,5 +1,6 @@
 """Registers all available default signals."""
 from ..embeddings.cohere import Cohere
+from ..embeddings.openai import OpenAI
 from ..embeddings.sbert import SBERT
 from .concept_scorer import ConceptScoreSignal
 from .pii import PIISignal
@@ -19,3 +20,4 @@ def register_default_signals() -> None:
   # Embeddings.
   register_signal(Cohere)
   register_signal(SBERT)
+  register_signal(OpenAI)
