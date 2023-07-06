59f9795bc9011c146f40ba6a2374f0a94d142e13
nsthorat <nsthorat@gmail.com>
2023-07-06 15:06:28 -0400

    save

--- src/signals/ner.py
+++ src/signals/ner.py
@@ -1,13 +1,35 @@
 """Compute named entity recognition with SpaCy."""
-from typing import Iterable, Optional
+from importlib import import_module
+from typing import Any, Iterable, Optional
 
 import spacy
 from pydantic import Field as PydanticField
+from spacy import Language
+from spacy.cli import download
 from typing_extensions import override
 
 from ..data.dataset_utils import lilac_span
 from ..schema import Field, Item, RichData, SignalInputType, field
 from .signal import TextSignal
+
+
+# Taken from https://github.com/BramVanroy/spacy_download/blob/main/src/spacy_download/load.py
+def load_spacy(model_name: str, **kwargs: dict[Any, Any]) -> Language:
+  """Load a spaCy model, download it if it has not been installed yet.
+
+  :param model_name: the model name, e.g., en_core_web_sm
+  :param kwargs: options passed to the spaCy loader, such as component exclusion, as you
+  would with spacy.load()
+  :return: an initialized spaCy Language
+  :raises: SystemExit: if the model_name cannot be downloaded.
+  """
+  try:
+    model_module = import_module(model_name)
+  except ModuleNotFoundError:
+    download(model_name)
+    model_module = import_module(model_name)
+
+  return model_module.load(**kwargs)
 
 
 class SpacyNER(TextSignal):
@@ -28,7 +50,7 @@ class SpacyNER(TextSignal):
 
   @override
   def setup(self) -> None:
-    self._nlp = spacy.load(
+    self._nlp = load_spacy(
       'en_core_web_sm',
       # Disable everything except the NER component. See: https://spacy.io/models
       disable=['tok2vec', 'tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
