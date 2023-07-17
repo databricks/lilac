56316413154ddeadac42b1555d55ca030acae6e9
Daniel Smilkov <dsmilkov@gmail.com>
2023-07-17 11:35:38 -0400

    save

--- src/signals/pii.py
+++ src/signals/pii.py
@@ -1,7 +1,9 @@
 """Compute text statistics for a document."""
+import ipaddress
 import re
-from typing import Iterable, Optional
+from typing import Iterable, Iterator, Optional
 
+import regex
 from typing_extensions import override
 
 from ..data.dataset_utils import lilac_span
@@ -9,6 +11,7 @@ from ..schema import Field, Item, RichData, SignalInputType, field
 from .signal import TextSignal
 
 EMAILS_KEY = 'emails'
+IPS_KEY = 'ip_addresses'
 NUM_EMAILS_KEY = 'num_emails'
 
 # This regex is a fully RFC 5322 regex for email addresses.
@@ -37,4 +40,83 @@ class PIISignal(TextSignal):
         yield None
         continue
       emails = [lilac_span(m.start(0), m.end(0)) for m in EMAIL_REGEX.finditer(text)]
-      yield {EMAILS_KEY: emails, NUM_EMAILS_KEY: len(emails)}
+      ips = list(_get_ips(text))
+      yield {EMAILS_KEY: emails, NUM_EMAILS_KEY: len(emails), IPS_KEY: ips}
+
+
+# Code below is forked from
+# https://github.com/bigcode-project/pii-lib/blob/main/utils/emails_ip_addresses_detection.py
+# under the Apache 2.0 License.
+
+ipv4_pattern = r'(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'  # noqa: E501
+ipv6_pattern = r'(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])'  # noqa: E501
+ip_pattern = regex.compile(
+  (r'(?:^|[\b\s@?,!;:\'\")(.\p{Han}])(' + r'|'.join([ipv4_pattern, ipv6_pattern]) +
+   ')(?:$|[\\s@,?!;:\'\"(.\\p{Han}])'))
+
+year_patterns = [
+  regex.compile(
+    r"(?:^|[\b\s@?,!;:\'\")(.\p{Han}])([1-2][0-9]{3}[\p{Pd}/][1-2][0-9]{3})(?:$|[\s@,?!;:\'\"(.\p{Han}])"
+  ),  # yyyy-yyyy or yyyy/yyyy
+  regex.compile(
+    r"(?:^|[\b\s@?,!;:\'\")(.\p{Han}])([1-2][0-9]{3}[\p{Pd}/.][0-3][0-9][\p{Pd}/.][0-3][0-9])(?:$|[\s@,?!;:\'\"(.\p{Han}])"
+  ),  # yyyy-mm-dd or yyyy-dd-mm or yyyy/mm/dd or yyyy/dd/mm or yyyy.mm.dd or yyyy.dd.mm
+  regex.compile(
+    r"(?:^|[\b\s@?,!;:\'\")(.\p{Han}])([0-3][0-9][\p{Pd}/.][0-3][0-9][\p{Pd}/.](?:[0-9]{2}|[1-2][0-9]{3}))(?:$|[\s@,?!;:\'\"(.\p{Han}])"
+  ),  # mm-dd-yyyy or dd-mm-yyyy or mm/dd/yyyy or dd/mm/yyyy or mm.dd.yyyy or dd.mm.yyyy
+  regex.compile(
+    r"(?:^|[\b\s@?,!;:\'\")(.\p{Han}])([0-3][0-9][\p{Pd}/](?:[0-9]{2}|[1-2][0-9]{3}))(?:$|[\s@,?!;:\'\"(.\p{Han}])"
+  ),  # mm-yyyy or mm/yyyy or the same but with yy
+  regex.compile(
+    r"(?:^|[\b\s@?,!;:\'\")(.\p{Han}])([1-2][0-9]{3}-[0-3][0-9])(?:$|[\s@,?!;:\'\"(.\p{Han}])"
+  ),  # yyyy-mm or yyyy/mm
+]
+
+
+def _ip_has_digit(matched_str: str) -> bool:
+  """Checks to make sure the PII span is not just ::."""
+  return any(map(str.isdigit, matched_str))
+
+
+def _matches_date_pattern(matched_str: str) -> bool:
+  # Screen out date false positives.
+  for year_regex in year_patterns:
+    if year_regex.match(matched_str):
+      return True
+  return False
+
+
+def filter_versions(matched_str: str, context: str) -> bool:
+  """Filter x.x.x.x and the words dns/server don't appear in the context."""
+  # count occurrence of dots.
+  dot_count = matched_str.count('.')
+  exclude = (dot_count == 3 and len(matched_str) == 7)
+  if exclude:
+    if 'dns' in context.lower() or 'server' in context.lower():
+      return False
+  return exclude
+
+
+def not_ip_address(matched_str: str) -> bool:
+  """Make sure the string has a valid IP address format e.g: 33.01.33.33 is not a valid."""
+  try:
+    ipaddress.ip_address(matched_str)
+    return False
+  except ValueError:
+    return True
+
+
+def _get_ips(text: str) -> Iterator[Item]:
+  for match in ip_pattern.finditer(text):
+    if not match.groups():
+      continue
+    value = match.group(1)
+    start, end = match.span(1)
+    # Filter out false positive IPs
+    if not _ip_has_digit(value):
+      continue
+    if _matches_date_pattern(value):
+      continue
+    if filter_versions(value, text[start - 100:end + 100]) or not_ip_address(value):
+      continue
+    yield lilac_span(start, end)
--- src/signals/pii_test.py
+++ src/signals/pii_test.py
@@ -1,7 +1,7 @@
 """Test the PII signal."""
 
 from ..schema import field
-from .pii import EMAILS_KEY, NUM_EMAILS_KEY, PIISignal
+from .pii import EMAILS_KEY, IPS_KEY, NUM_EMAILS_KEY, PIISignal
 from .splitters.text_splitter_test_utils import text_to_expected_spans
 
 
@@ -18,7 +18,7 @@ def test_pii_compute() -> None:
 
   expected_spans = text_to_expected_spans(text, ['nik@test.com', 'pii@gmail.com'])
 
-  assert emails == [{EMAILS_KEY: expected_spans, NUM_EMAILS_KEY: 2}]
+  assert emails == [{EMAILS_KEY: expected_spans, NUM_EMAILS_KEY: 2, IPS_KEY: []}]
 
 
 def test_pii_case_insensitive() -> None:
@@ -29,4 +29,14 @@ def test_pii_case_insensitive() -> None:
 
   expected_spans = text_to_expected_spans(text, ['NIK@Test.com', 'pII@gmAIL.COM'])
 
-  assert emails == [{EMAILS_KEY: expected_spans, NUM_EMAILS_KEY: 2}]
+  assert emails == [{EMAILS_KEY: expected_spans, NUM_EMAILS_KEY: 2, IPS_KEY: []}]
+
+
+def test_ip_addresses() -> None:
+  signal = PIISignal()
+
+  text = 'These are some ip addresses: 192.158.1.38 and 2001:db8:3333:4444:5555:6666:7777:8888'
+  pii = list(signal.compute([text]))
+  expected_spans = text_to_expected_spans(
+    text, ['192.158.1.38', '2001:db8:3333:4444:5555:6666:7777:8888'])
+  assert pii == [{EMAILS_KEY: [], NUM_EMAILS_KEY: 0, IPS_KEY: expected_spans}]
