0fcecad6b1eb8153ff9e98d39fca34c477683501
Daniel Smilkov <dsmilkov@gmail.com>
2023-07-20 11:09:32 -0400

    save

--- src/signals/pii_secrets.py
+++ src/signals/pii_secrets.py
@@ -8,12 +8,8 @@ import os
 import tempfile
 from typing import Iterator
 
-try:
-  from detect_secrets import SecretsCollection
-  from detect_secrets.settings import transient_settings
-except ImportError:
-  raise ImportError('Could not import the "detect_secrets" python package. '
-                    'Please install it as optional dependency via `pip install lilac[pii]`.')
+from detect_secrets import SecretsCollection
+from detect_secrets.settings import transient_settings
 
 from ..data.dataset_utils import lilac_span
 from ..schema import Item
