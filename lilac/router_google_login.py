835377a0dae64bdac9fccb6bb2bd9bf808a8834d
nsthorat <nsthorat@gmail.com>
2023-07-24 19:41:05 -0400

    save

--- lilac/router_google_login.py
+++ lilac/router_google_login.py
@@ -14,30 +14,32 @@ from .router_utils import RouteErrorHandler
 
 router = APIRouter(route_class=RouteErrorHandler)
 
-_OAUTH: OAuth = OAuth(
-  Config(environ={
-    'GOOGLE_CLIENT_ID': env('GOOGLE_CLIENT_ID'),
-    'GOOGLE_CLIENT_SECRET': env('GOOGLE_CLIENT_SECRET')
-  }))
-_OAUTH.register(
-  name='google',
-  server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-  client_kwargs={'scope': 'openid email profile'},
-)
+if env('LILAC_AUTH_ENABLED'):
+  oauth = OAuth(
+    Config(
+      environ={
+        'GOOGLE_CLIENT_ID': env('GOOGLE_CLIENT_ID'),
+        'GOOGLE_CLIENT_SECRET': env('GOOGLE_CLIENT_SECRET')
+      }))
+  oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+  )
 
 
 @router.get('/login')
 async def login(request: Request, origin_url: str) -> RedirectResponse:
   """Redirects to Google OAuth login page."""
   auth_path = urlunparse(urlparse(origin_url)._replace(path='/google/auth'))
-  return await _OAUTH.google.authorize_redirect(request, auth_path)
+  return await oauth.google.authorize_redirect(request, auth_path)
 
 
 @router.get('/auth')
 async def auth(request: Request) -> Response:
   """Handles the Google OAuth callback."""
   try:
-    token = await _OAUTH.google.authorize_access_token(request)
+    token = await oauth.google.authorize_access_token(request)
   except OAuthError as error:
     return HTMLResponse(f'<h1>{error}</h1>')
   userinfo = token['userinfo']
