167852ea46a5a09af883e89eed73fdeba828a963
Nikhil Thorat <nsthorat@gmail.com>
2023-08-29 09:42:56 -0400

    save

--- scripts/publish_pip.sh
+++ scripts/publish_pip.sh
@@ -10,7 +10,24 @@ if [[ -z "${PYPI_TOKEN}" ]]; then
   exit 1
 fi
 
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$CUR_BRANCH" != "nik-publish" ]]; then
+  echo "Please checkout the main branch before publishing."
+  exit 1
+fi
+
+# Build the web server.
+./scripts/build_server_prod.sh
+
+exit 1
+
 poetry version patch
+
+NEW_VERSION=$(poetry version --short)
+
+gh pr create --title "Bump version to $NEW_VERSION" --web
+
 poetry config pypi-token.pypi $PYPI_TOKEN
 poetry build
 
@@ -18,6 +35,11 @@ read -p "Continue (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
   poetry publish
   echo "Published $(poetry version)"
+
+  echo "Please run ./scripts/tag_version.sh to tag the version after the PR is committed."
 else
   echo "Did not publish"
+  exit 1
 fi
+
+gh release create
