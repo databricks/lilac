f1022b6a368e250d333040fc503448b0e738802e
Nikhil Thorat <nsthorat@gmail.com>
2023-09-05 12:40:46 -0400

    save

--- lilac/data/dataset_duckdb.py
+++ lilac/data/dataset_duckdb.py
@@ -1666,12 +1666,14 @@ def _merge_cells(dest_cell: Item, source_cell: Item) -> Item:
     else:
       # Primitives can be merged together if they are equal. This can happen if a user selects a
       # column that is the child of another.
+      # NOTE: This can be removed if we fix https://github.com/lilacai/lilac/issues/166.
       if isinstance(dest_cell, float):
+        # For floats, explicitly check closeness as precision issues can lead to them not being
+        # exactly equal.
         if not math.isclose(source_cell, dest_cell, abs_tol=1e-5):
           raise ValueError(
             f'Cannot merge source "{source_cell!r}" into destination "{dest_cell!r}"')
       else:
-        # NOTE: This can be removed if we fix https://github.com/lilacai/lilac/issues/166.
         if source_cell != dest_cell:
           raise ValueError(
             f'Cannot merge source "{source_cell!r}" into destination "{dest_cell!r}"')
