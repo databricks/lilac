d3e0a81f196a54fa8b76e135fd5967b81fe13a59
Nikhil Thorat <nsthorat@gmail.com>
2023-09-14 17:24:41 -0400

    save

---
+++ web/lib/fastapi_client/models/AuthenticationInfo.ts
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { UserAccess } from './UserAccess';
+import type { UserInfo } from './UserInfo';
+
+/**
+ * Authentication information for the user.
+ */
+export type AuthenticationInfo = {
+    user?: UserInfo;
+    access: UserAccess;
+    auth_enabled: boolean;
+    huggingface_space_id?: string;
+};
+
---
+++ web/lib/fastapi_client/models/Column.ts
@@ -0,0 +1,21 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ConceptLabelsSignal } from './ConceptLabelsSignal';
+import type { ConceptSignal } from './ConceptSignal';
+import type { SemanticSimilaritySignal } from './SemanticSimilaritySignal';
+import type { Signal } from './Signal';
+import type { SubstringSignal } from './SubstringSignal';
+import type { TextEmbeddingSignal } from './TextEmbeddingSignal';
+import type { TextSignal } from './TextSignal';
+
+/**
+ * A column in the dataset.
+ */
+export type Column = {
+    path: Array<string>;
+    alias?: string;
+    signal_udf?: (ConceptSignal | ConceptLabelsSignal | SubstringSignal | SemanticSimilaritySignal | TextEmbeddingSignal | TextSignal | Signal);
+};
+
