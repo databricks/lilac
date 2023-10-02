01fd062fb4ae7c2c20f198ead4e0f046e988e6a9
Nikhil Thorat <nsthorat@gmail.com>
2023-10-02 13:34:05 -0400

    save

--- web/lib/fastapi_client/services/RagService.ts
+++ web/lib/fastapi_client/services/RagService.ts
@@ -9,18 +9,18 @@ import { request as __request } from '../core/request';
 export class RagService {
 
     /**
-     * Generate Answer
-     * Generate positive examples for a given concept using an LLM model.
+     * Generate Completion
+     * Generate the completion for a prompt.
      * @param prompt
      * @returns string Successful Response
      * @throws ApiError
      */
-    public static generateAnswer(
+    public static generateCompletion(
         prompt: string,
     ): CancelablePromise<string> {
         return __request(OpenAPI, {
             method: 'GET',
-            url: '/api/v1/rag/generate_answer',
+            url: '/api/v1/rag/generate_completion',
             query: {
                 'prompt': prompt,
             },
