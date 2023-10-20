3d570ea4595b106de6742f3dc94a7aab9d84a778
Daniel Smilkov <dsmilkov@gmail.com>
2023-10-20 11:15:36 -0400

    save

--- lilac/sources/parquet_source.py
+++ lilac/sources/parquet_source.py
@@ -3,7 +3,7 @@ from typing import ClassVar, Iterable, Optional, cast
 
 import duckdb
 import pyarrow as pa
-from pydantic import Field
+from pydantic import Field, field_validator
 from typing_extensions import override
 
 from ..schema import Item, arrow_schema_to_schema
@@ -24,10 +24,28 @@ class ParquetSource(Source):
   filepaths: list[str] = Field(
     description=
     'A list of paths to parquet files which live locally or remotely on GCS, S3, or Hadoop.')
+  sample_size: Optional[int] = Field(
+    title='Sample size', description='Number of rows to sample from the dataset', default=None)
 
   _source_schema: Optional[SourceSchema] = None
   _reader: Optional[pa.RecordBatchReader] = None
   _con: Optional[duckdb.DuckDBPyConnection] = None
+
+  @field_validator('filepaths')
+  @classmethod
+  def validate_filepaths(cls, filepaths: list[str]) -> list[str]:
+    """Validate filepaths."""
+    if not filepaths:
+      raise ValueError('filepaths must be non-empty.')
+    return filepaths
+
+  @field_validator('sample_size')
+  @classmethod
+  def validate_sample_size(cls, sample_size: int) -> int:
+    """Validate sample size."""
+    if sample_size < 1:
+      raise ValueError('sample_size must be greater than 0.')
+    return sample_size
 
   @override
   def setup(self) -> None:
@@ -39,8 +57,9 @@ class ParquetSource(Source):
     s3_filepaths = [path.replace('gs://', 's3://') for path in filepaths]
 
     # NOTE: We use duckdb here to increase parallelism for multiple files.
+    sample_suffix = f'USING SAMPLE {self.sample_size}' if self.sample_size else ''
     self._con.execute(f"""
-      CREATE VIEW t as (SELECT * FROM read_parquet({s3_filepaths}));
+      CREATE VIEW t as (SELECT * FROM read_parquet({s3_filepaths}) {sample_suffix});
     """)
     res = self._con.execute('SELECT COUNT(*) FROM t').fetchone()
     num_items = cast(tuple[int], res)[0]
