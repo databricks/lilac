45bde9439fa498be978e1951279dfc9ccc32d524
Daniel Smilkov <dsmilkov@gmail.com>
2023-10-20 10:50:38 -0400

    save

--- lilac/sources/parquet_source.py
+++ lilac/sources/parquet_source.py
@@ -1,13 +1,15 @@
 """Parquet source."""
-from typing import ClassVar, Iterable, Optional
+from typing import ClassVar, Iterable, Optional, cast
 
+import duckdb
 import pyarrow as pa
-import pyarrow.parquet as pq
 from pydantic import Field
 from typing_extensions import override
 
 from ..schema import Item, arrow_schema_to_schema
 from ..source import Source, SourceSchema
+from ..sources.duckdb_utils import duckdb_setup
+from ..utils import download_http_files
 
 
 class ParquetSource(Source):
@@ -24,15 +26,28 @@ class ParquetSource(Source):
     'A list of paths to parquet files which live locally or remotely on GCS, S3, or Hadoop.')
 
   _source_schema: Optional[SourceSchema] = None
-  _table: Optional[pa.Table] = None
+  _reader: Optional[pa.RecordBatchReader] = None
+  _con: Optional[duckdb.DuckDBPyConnection] = None
 
   @override
   def setup(self) -> None:
-    assert self.filepaths, 'filepaths must be specified.'
-    self._table = pa.concat_tables([pq.read_table(f) for f in self.filepaths])
-    self._source_schema = SourceSchema(
-      fields=arrow_schema_to_schema(pq.read_schema(self.filepaths[0])).fields,
-      num_items=self._table.num_rows)
+    filepaths = download_http_files(self.filepaths)
+    self._con = duckdb.connect(database=':memory:')
+    duckdb_setup(self._con)
+
+    # DuckDB expects s3 protocol: https://duckdb.org/docs/guides/import/s3_import.html.
+    s3_filepaths = [path.replace('gs://', 's3://') for path in filepaths]
+
+    # NOTE: We use duckdb here to increase parallelism for multiple files.
+    self._con.execute(f"""
+      CREATE VIEW t as (SELECT * FROM read_parquet({s3_filepaths}));
+    """)
+    res = self._con.execute('SELECT COUNT(*) FROM t').fetchone()
+    num_items = cast(tuple[int], res)[0]
+    self._reader = self._con.execute('SELECT * from t').fetch_record_batch(rows_per_batch=10_000)
+    # Create the source schema in prepare to share it between process and source_schema.
+    schema = arrow_schema_to_schema(self._reader.schema)
+    self._source_schema = SourceSchema(fields=schema.fields, num_items=num_items)
 
   @override
   def source_schema(self) -> SourceSchema:
@@ -43,6 +58,10 @@ class ParquetSource(Source):
   @override
   def process(self) -> Iterable[Item]:
     """Process the source."""
-    assert self._table is not None, 'setup() must be called first.'
-    for row in self._table.to_pylist():
-      yield row
+    assert self._reader and self._con, 'setup() must be called first.'
+
+    for batch in self._reader:
+      yield from batch.to_pylist()
+
+    self._reader.close()
+    self._con.close()
