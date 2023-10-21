7947e56b46f02eec97df2b6047d24444f60bb0c9
Daniel Smilkov <dsmilkov@gmail.com>
2023-10-21 18:47:15 -0400

    save

--- lilac/sources/parquet_source.py
+++ lilac/sources/parquet_source.py
@@ -1,12 +1,15 @@
 """Parquet source."""
+import os
+import random
 from typing import ClassVar, Iterable, Optional, cast
 
 import duckdb
 import pyarrow as pa
+from fsspec.core import url_to_fs
 from pydantic import Field, field_validator
 from typing_extensions import override
 
-from ..schema import Item, arrow_schema_to_schema
+from ..schema import Item, Schema, arrow_schema_to_schema
 from ..source import Source, SourceSchema
 from ..sources.duckdb_utils import duckdb_setup
 from ..utils import download_http_files
@@ -29,9 +32,14 @@ class ParquetSource(Source):
     'A list of paths to parquet files which live locally or remotely on GCS, S3, or Hadoop.')
   sample_size: Optional[int] = Field(
     title='Sample size', description='Number of rows to sample from the dataset', default=None)
+  shuffle_between_shards: bool = Field(
+    default=False,
+    description=
+    'If true, the sampling method will stream data from  shards, but not shuffle inside each shard.'
+  )
 
   _source_schema: Optional[SourceSchema] = None
-  _reader: Optional[pa.RecordBatchReader] = None
+  _readers: list[pa.RecordBatchReader] = []
   _con: Optional[duckdb.DuckDBPyConnection] = None
 
   @field_validator('filepaths')
@@ -50,6 +58,26 @@ class ParquetSource(Source):
       raise ValueError('sample_size must be greater than 0.')
     return sample_size
 
+  def _setup_sampling(self, filepaths: list[str]) -> Schema:
+    assert self._con, 'setup() must be called first.'
+    batch_size = ROWS_PER_BATCH_READ
+    files: list[str] = []
+    if self.sample_size:
+      # Find the number of files.
+      for filepath in filepaths:
+        fs, _ = url_to_fs(filepath)
+        files.extend(fs.glob(filepath))
+      batch_size = min(self.sample_size // len(files), batch_size)
+    if self.shuffle_between_shards:
+      for file in files:
+        res = self._con.cursor().execute(f"""SELECT * FROM read_parquet('{file}')""")
+        self._readers.append(res.fetch_record_batch(rows_per_batch=batch_size))
+    else:
+      sample_suffix = f'USING SAMPLE {self.sample_size}' if self.sample_size else ''
+      res = self._con.execute(f"""SELECT * FROM read_parquet({filepaths}) {sample_suffix}""")
+      self._readers.append(res.fetch_record_batch(rows_per_batch=batch_size))
+    return arrow_schema_to_schema(self._readers[0].schema)
+
   @override
   def setup(self) -> None:
     filepaths = download_http_files(self.filepaths)
@@ -57,19 +85,13 @@ class ParquetSource(Source):
     duckdb_setup(self._con)
 
     # DuckDB expects s3 protocol: https://duckdb.org/docs/guides/import/s3_import.html.
-    s3_filepaths = [path.replace('gs://', 's3://') for path in filepaths]
-
-    # NOTE: We use duckdb here to increase parallelism for multiple files.
-    sample_suffix = f'USING SAMPLE {self.sample_size}' if self.sample_size else ''
-    self._con.execute(f"""
-      CREATE VIEW t as (SELECT * FROM read_parquet({s3_filepaths}) {sample_suffix});
-    """)
-    res = self._con.execute('SELECT COUNT(*) FROM t').fetchone()
+    filepaths = [path.replace('gs://', 's3://') for path in filepaths]
+    res = self._con.execute(f'SELECT COUNT(*) FROM read_parquet({filepaths})').fetchone()
     num_items = cast(tuple[int], res)[0]
-    self._reader = self._con.execute('SELECT * from t').fetch_record_batch(
-      rows_per_batch=ROWS_PER_BATCH_READ)
-    # Create the source schema in prepare to share it between process and source_schema.
-    schema = arrow_schema_to_schema(self._reader.schema)
+    if self.sample_size:
+      self.sample_size = min(self.sample_size, num_items)
+      num_items = self.sample_size
+    schema = self._setup_sampling(filepaths)
     self._source_schema = SourceSchema(fields=schema.fields, num_items=num_items)
 
   @override
@@ -81,10 +103,30 @@ class ParquetSource(Source):
   @override
   def process(self) -> Iterable[Item]:
     """Process the source."""
-    assert self._reader and self._con, 'setup() must be called first.'
+    assert self._con, 'setup() must be called first.'
 
-    for batch in self._reader:
-      yield from batch.to_pylist()
+    items_yielded = 0
+    done = False
+    while not done:
+      index = random.randint(0, len(self._readers) - 1)
+      reader = self._readers[index]
+      batch = None
+      try:
+        batch = reader.read_next_batch()
+      except StopIteration:
+        del self._readers[index]
+        if not self._readers:
+          done = True
+          break
+        continue
+      items = batch.to_pylist()
+      for item in items:
+        yield item
+        items_yielded += 1
+        if self.sample_size and items_yielded == self.sample_size:
+          done = True
+          break
 
-    self._reader.close()
+    for reader in self._readers:
+      reader.close()
     self._con.close()
--- lilac/sources/parquet_source_test.py
+++ lilac/sources/parquet_source_test.py
@@ -10,6 +10,7 @@ from pydantic import ValidationError
 
 from ..schema import schema
 from ..source import SourceSchema
+from ..utils import chunks
 from .parquet_source import ParquetSource
 
 
@@ -42,25 +43,82 @@ def test_simple_rows(tmp_path: pathlib.Path) -> None:
 
 
 def test_sampling(tmp_path: pathlib.Path) -> None:
-  table = pa.Table.from_pylist([{
-    'name': 'a',
-    'age': 1
-  }, {
-    'name': 'b',
-    'age': 2
-  }, {
-    'name': 'c',
-    'age': 3
-  }])
+  source_items = [{'name': 'a', 'age': 1}, {'name': 'b', 'age': 2}, {'name': 'c', 'age': 3}]
+  table = pa.Table.from_pylist(source_items)
 
   out_file = os.path.join(tmp_path, 'test.parquet')
   pq.write_table(table, out_file)
 
-  for sample_size in range(1, 4):
+  # Test sampling with different sample sizes, including sample size > num_items.
+  for sample_size in range(1, 5):
     source = ParquetSource(filepaths=[out_file], sample_size=sample_size)
     source.setup()
     items = list(source.process())
-    assert len(items) == sample_size
+    assert len(items) == min(sample_size, len(source_items))
+
+
+def test_read_multiple_shards(tmp_path: pathlib.Path) -> None:
+  source_items = [{'name': 'a', 'age': 1}, {'name': 'b', 'age': 2}, {'name': 'c', 'age': 3}]
+  for i, item in enumerate(source_items):
+    table = pa.Table.from_pylist([item])
+    out_file = tmp_path / f'test-{i}.parquet'
+    pq.write_table(table, out_file)
+
+  source = ParquetSource(filepaths=[str(tmp_path / 'test-*.parquet')])
+  source.setup()
+  items = list(source.process())
+  assert items == source_items
+
+
+def test_shuffle_between_shards(tmp_path: pathlib.Path) -> None:
+  source_items = [{'name': 'a', 'age': 1}, {'name': 'b', 'age': 2}, {'name': 'c', 'age': 3}]
+  for i, item in enumerate(source_items):
+    table = pa.Table.from_pylist([item])
+    out_file = tmp_path / f'test-{i}.parquet'
+    pq.write_table(table, out_file)
+
+  # Test sampling with different sample sizes, including sample size > num_items.
+  for sample_size in range(1, 5):
+    source = ParquetSource(
+      filepaths=[str(tmp_path / 'test-*.parquet')],
+      shuffle_between_shards=True,
+      sample_size=sample_size)
+    source.setup()
+    items = list(source.process())
+    assert len(items) == min(sample_size, len(source_items))
+
+
+def test_shuffle_between_uniform_shards(tmp_path: pathlib.Path) -> None:
+  source_items = [{'index': i} for i in range(100)]
+  for i, chunk in enumerate(chunks(source_items, 10)):
+    table = pa.Table.from_pylist(chunk)
+    out_file = tmp_path / f'test-{i}.parquet'
+    pq.write_table(table, out_file)
+
+  source = ParquetSource(
+    filepaths=[str(tmp_path / 'test-*.parquet')], shuffle_between_shards=True, sample_size=20)
+  source.setup()
+  items = list(source.process())
+  assert len(items) == 20
+  for item in items:
+    assert item['index'] % 10 < 2
+
+
+def test_shuffle_between_nonuniform_shards(tmp_path: pathlib.Path) -> None:
+  source_items = [{'index': i} for i in range(100)]
+  shard_sizes = [49, 1, 40, 10]
+  for i, shard_size in enumerate(shard_sizes):
+    chunk = source_items[:shard_size]
+    source_items = source_items[shard_size:]
+    table = pa.Table.from_pylist(chunk)
+    out_file = tmp_path / f'test-{i}.parquet'
+    pq.write_table(table, out_file)
+
+  source = ParquetSource(
+    filepaths=[str(tmp_path / 'test-*.parquet')], shuffle_between_shards=True, sample_size=20)
+  source.setup()
+  items = list(source.process())
+  assert len(items) == 20
 
 
 def test_validation() -> None:
