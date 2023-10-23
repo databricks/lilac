6d06715027ea276bc865c63f825f507c2dc591a2
Nikhil Thorat <nsthorat@gmail.com>
2023-10-23 12:11:07 -0400

    save

--- web/blueprint/src/lib/components/datasetView/ExportModal.svelte
+++ web/blueprint/src/lib/components/datasetView/ExportModal.svelte
@@ -26,10 +26,9 @@
     SkeletonText,
     TextArea,
     TextInput,
-    ToastNotification,
     Toggle
   } from 'carbon-components-svelte';
-  import {Tag} from 'carbon-icons-svelte';
+  import {Download, Tag} from 'carbon-icons-svelte';
   import {createEventDispatcher} from 'svelte';
   import FieldList from './FieldList.svelte';
   export let open = false;
@@ -202,16 +201,20 @@
         {:else if $exportDataset.isLoading}
           <SkeletonPlaceholder />
         {:else if $exportDataset.data}
-          <InlineNotification kind="success" lowContrast hideCloseButton title="Dataset exported">
-            <div slot="subtitle">
-              Saved to <a href={downloadUrl()}>{$exportDataset.data}</a>
-            </div>
-            <svelte:fragment slot="actions">
-              <NotificationActionButton on:click={() => window.open(downloadUrl(), '_blank')}>
-                Download
-              </NotificationActionButton>
-            </svelte:fragment>
-          </InlineNotification>
+          <div class="export-success">
+            <InlineNotification kind="success" lowContrast hideCloseButton>
+              <div slot="title" class="inline">
+                Dataset exported to <a href={downloadUrl()}>{$exportDataset.data}</a>
+              </div>
+              <svelte:fragment slot="actions">
+                <div class="my-auto">
+                  <NotificationActionButton on:click={() => window.open(downloadUrl(), '_blank')}>
+                    Download <span class="ml-2"><Download /></span>
+                  </NotificationActionButton>
+                </div>
+              </svelte:fragment>
+            </InlineNotification>
+          </div>
         {/if}
       </section>
       <section>
@@ -225,14 +228,12 @@
           {#if $previewRows && $previewRows.isFetching}
             <SkeletonText paragraph />
           {:else if previewRows && $previewRows}
-            <ToastNotification
+            <InlineNotification
               hideCloseButton
               kind="info"
-              fullWidth
               lowContrast
-              title={'Preview'}
-              caption={`The JSON representation of the selection below is not identical to the ` +
-                `contents of the exported file, which depends on the format selected above.`}
+              title={`The preview below is not identical to the exported file which depends on ` +
+                `the format selected above.`}
             />
             <TextArea
               value={JSON.stringify($previewRows.data, null, 2)}
@@ -255,6 +256,9 @@
 </ComposedModal>
 
 <style lang="postcss">
+  :global(.export-success .bx--inline-notification__text-wrapper) {
+    @apply w-full;
+  }
   h2 {
     @apply mb-2 border-b border-gray-300 pb-2;
   }
