25e4bebda57b8684384179a1f5448b7a0ad6a882
Brian Lee <brian@lilacml.com>
2023-11-08 14:05:45 -0500

    customize nlp

--- lilac/signals/pii_presidio.py
+++ lilac/signals/pii_presidio.py
@@ -1,4 +1,7 @@
-import presidio_analyzer
+"""Wrap Presidio library for detecting PII in text."""
+from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer.nlp_engine import SpacyNlpEngine
+from presidio_analyzer.nlp_engine.ner_model_configuration import NerModelConfiguration
 
 from ..schema import Item, lilac_span
 
@@ -10,18 +13,40 @@ PII_CATEGORIES = [
   'EMAIL_ADDRESS',
   'IBAN_CODE',
   'IP_ADDRESS',
-  'PERSON',
   'PHONE_NUMBER',
   'MEDICAL_LICENSE',
 ]
 
-ANALYZER = presidio_analyzer.AnalyzerEngine()
+
+# When SpaCy detects certain entities, but Presidio doesn't know what to do with them, it generates
+# logspam. Explicitly ignore these entities to suppress the logspam.
+IGNORED_SPACY_ENTITIES = [
+  'CARDINAL',
+  'EVENT',
+  'FAC',
+  'LANGUAGE',
+  'LAW',
+  'MONEY',
+  'ORDINAL',
+  'PERCENT',
+  'PRODUCT',
+  'QUANTITY',
+  'WORK_OF_ART',
+]
+
+SPACY_ENGINE = SpacyNlpEngine(
+  models=[{'lang_code': 'en', 'model_name': 'en_core_web_sm'}],
+  ner_model_configuration=NerModelConfiguration(
+    labels_to_ignore=IGNORED_SPACY_ENTITIES,
+  ),
+)
+ANALYZER = AnalyzerEngine(nlp_engine=SPACY_ENGINE)
 
 
 def find_pii(text: str) -> dict[str, list[Item]]:
   """Find personally identifiable information (emails, phone numbers, etc)."""
   results = ANALYZER.analyze(text, entities=PII_CATEGORIES, language='en')
-  pii_dict = {cat: [] for cat in PII_CATEGORIES}
+  pii_dict: dict[str, list[Item]] = {cat: [] for cat in PII_CATEGORIES}
   for result in results:
     pii_dict[result.entity_type].append(lilac_span(result.start, result.end))
   return pii_dict
