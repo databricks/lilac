b34aa0affb7220e4dedde81b6a9ef08e2bc69e06
Daniel Smilkov <dsmilkov@gmail.com>
2023-11-14 11:29:43 -0500

    save

--- web/blueprint/src/lib/components/datasetView/SingleItemView.svelte
+++ web/blueprint/src/lib/components/datasetView/SingleItemView.svelte
@@ -1,63 +1,48 @@
 <script lang="ts">
   import {
     queryDatasetManifest,
-    querySelectRows,
     querySelectRowsSchema,
     querySettings
   } from '$lib/queries/datasetQueries';
-  import {
-    getDatasetViewContext,
-    getSelectRowsOptions,
-    getSelectRowsSchemaOptions
-  } from '$lib/stores/datasetViewStore';
+  import {getDatasetViewContext, getSelectRowsSchemaOptions} from '$lib/stores/datasetViewStore';
   import {getHighlightedFields, getMediaFields} from '$lib/view_utils';
-  import {L, ROWID, formatValue} from '$lilac';
+  import {L, ROWID, formatValue, type SelectRowsResponse} from '$lilac';
   import {SkeletonText} from 'carbon-components-svelte';
   import {ChevronLeft, ChevronRight} from 'carbon-icons-svelte';
   import FilterPanel from './FilterPanel.svelte';
   import PrefetchRowItem from './PrefetchRowItem.svelte';
   import RowItem from './RowItem.svelte';
+  import SingleItemSelectRows from './SingleItemSelectRows.svelte';
 
   const store = getDatasetViewContext();
   const DEFAULT_LIMIT_SELECT_ROW_IDS = 5;
 
   let limit = DEFAULT_LIMIT_SELECT_ROW_IDS;
+  let rowsResponse: SelectRowsResponse | undefined;
+  let nextRowsResponse: SelectRowsResponse | undefined;
 
   $: selectRowsSchema = querySelectRowsSchema(
     $store.namespace,
     $store.datasetName,
     getSelectRowsSchemaOptions($store)
   );
-  $: selectOptions = getSelectRowsOptions($store, true /* implicitSortByRowID */);
-  $: rowsQuery = querySelectRows(
-    $store.namespace,
-    $store.datasetName,
-    {...selectOptions, columns: [ROWID], limit},
-    $selectRowsSchema.data?.schema
-  );
-  $: nextRowsQuery = querySelectRows(
-    $store.namespace,
-    $store.datasetName,
-    {...selectOptions, columns: [ROWID], limit: limit * 2},
-    $selectRowsSchema.data?.schema
-  );
 
-  $: rows = $rowsQuery?.data?.rows != null ? [...$rowsQuery.data.rows] : undefined;
+  $: rows = rowsResponse?.rows;
 
   $: manifest = queryDatasetManifest($store.namespace, $store.datasetName);
-  $: rowId = $store.rowId || L.value(rows?.[0]?.[ROWID], 'string');
+  $: rowId = $store.rowId || L.value(rows?.[0][ROWID], 'string');
 
   // Find the index if the row id is known.
   $: index =
     rowId != null && rows != null
-      ? rows.findIndex(row => L.value(row[ROWID], 'string') === $store.rowId)
+      ? rows.findIndex(row => L.value(row[ROWID], 'string') === rowId)
       : undefined;
 
   // Double the limit of select rows if the row id was not found.
   $: rowIdWasNotFound = rows != null && index != null && (index === -1 || index >= rows.length);
   $: limit =
-    rowIdWasNotFound && $rowsQuery?.data?.total_num_rows
-      ? Math.min(limit * 2, $rowsQuery.data.total_num_rows)
+    rowIdWasNotFound && rowsResponse?.total_num_rows
+      ? Math.min(limit * 2, rowsResponse.total_num_rows)
       : limit;
 
   $: settings = querySettings($store.namespace, $store.datasetName);
@@ -71,7 +56,7 @@
       return;
     }
     const newIndex = next ? index + 1 : Math.max(index - 1, 0);
-    const newRowId = L.value($nextRowsQuery.data?.rows[newIndex][ROWID], 'string');
+    const newRowId = L.value(nextRowsResponse?.rows[newIndex][ROWID], 'string');
     if (newRowId != null) {
       store.setRowId(newRowId);
       return;
@@ -87,7 +72,7 @@
   }
 </script>
 
-<FilterPanel totalNumRows={$rowsQuery?.data?.total_num_rows} manifest={$manifest.data} />
+<FilterPanel totalNumRows={rowsResponse?.total_num_rows} manifest={$manifest.data} />
 
 <div
   class="mx-5 my-2 flex items-center justify-between rounded-lg border border-neutral-300 bg-neutral-100 py-2"
@@ -113,8 +98,8 @@
       </span>
       of
       <span class="inline-flex">
-        {#if $rowsQuery?.data?.total_num_rows != null}
-          {formatValue($rowsQuery?.data?.total_num_rows)}
+        {#if rowsResponse?.total_num_rows != null}
+          {formatValue(rowsResponse?.total_num_rows)}
         {:else}
           <SkeletonText lines={1} class="!w-20" />
         {/if}
@@ -122,13 +107,16 @@
     </div>
   </div>
   <div class="flex-0">
-    {#if index != null && index < ($rowsQuery?.data?.total_num_rows || 0) - 1}
+    {#if index != null && index < (rowsResponse?.total_num_rows || 0) - 1}
       <button on:click={() => updateRowId(true)}>
         <ChevronRight title="Next item" size={24} />
       </button>
     {/if}
   </div>
 </div>
+
+<SingleItemSelectRows {limit} bind:rowsResponse />
+<SingleItemSelectRows limit={limit * 2} bind:rowsResponse={nextRowsResponse} />
 
 {#each rows || [] as row}
   {@const rowId = L.value(row[ROWID], 'string')}
@@ -141,4 +129,4 @@
   </div>
 {/if}
 
-<svelte:window on:keydown|preventDefault={onKeyDown} />
+<svelte:window on:keydown={onKeyDown} />
