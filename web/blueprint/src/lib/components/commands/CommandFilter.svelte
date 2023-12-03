4eea6c4b2ff5cfa5b92a5b397bbcb3fe31fb5a6b
Daniel Smilkov <dsmilkov@gmail.com>
2023-12-03 18:16:47 -0500

    save

--- web/blueprint/src/lib/components/commands/CommandFilter.svelte
+++ web/blueprint/src/lib/components/commands/CommandFilter.svelte
@@ -95,7 +95,9 @@
   }
 
   function submit() {
-    $datasetViewStore.query.filters = stagedFilters;
+    for (const filter of stagedFilters) {
+      datasetViewStore.addFilter(filter);
+    }
     close();
   }
 </script>
