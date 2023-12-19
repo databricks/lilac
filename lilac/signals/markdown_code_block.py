28f5045b7b9678f479eb290c95f133647067b114
Nikhil Thorat <nsthorat@gmail.com>
2023-12-18 20:25:04 -0500

    save

--- lilac/signals/markdown_code_block.py
+++ lilac/signals/markdown_code_block.py
@@ -11,14 +11,14 @@ from typing_extensions import override
 from ..schema import Field, Item, RichData, field, span
 from ..signal import TextSignal
 
+MARKDOWN_RE = '```([^\n ]*?)\n(.*?)\n```'
+
 
 class MarkdownCodeBlockSignal(TextSignal):
   """Finds markdown blocks in text. Emits the language of the block with the span."""
 
   name: ClassVar[str] = 'markdown_code_block'
   display_name: ClassVar[str] = 'Markdown Code Block Detection'
-
-  markdown_block_re = re.compile('```([^\n ]*?)\n(.*?)\n```', re.MULTILINE | re.DOTALL)
 
   @override
   def fields(self) -> Field:
@@ -33,11 +33,12 @@ class MarkdownCodeBlockSignal(TextSignal):
 
   @override
   def compute(self, data: Iterable[RichData]) -> Iterator[Optional[Item]]:
+    markdown_re = re.compile(MARKDOWN_RE, re.MULTILINE | re.DOTALL)
     for doc in data:
       text = cast(str, doc)
       # Get the spans
-      markdown_re_spans = self.markdown_block_re.finditer(text)
-      languages = self.markdown_block_re.findall(text)
+      markdown_re_spans = markdown_re.finditer(text)
+      languages = markdown_re.findall(text)
 
       spans: list[Item] = []
       for re_span, (language, _) in zip(markdown_re_spans, languages):
