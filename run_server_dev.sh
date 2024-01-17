d1edce4a715164e3647b360f0d76cbf2df91e1f2
Daniel Smilkov <dsmilkov@gmail.com>
2024-01-16 23:05:47 -0500

    save

--- run_server_dev.sh
+++ run_server_dev.sh
@@ -4,7 +4,7 @@ set -e
 export NODE_ENV=development
 
 # Make the web client upon bootup to make sure TypeScript files are in sync.
-# poetry run python -m scripts.make_fastapi_client
+poetry run python -m scripts.make_fastapi_client
 
 # Start the vite devserver.
 npm run dev --workspace web/blueprint -- --open &
@@ -16,14 +16,14 @@ poetry run uvicorn lilac.server:app --reload --port 5432 --host 0.0.0.0 \
   --reload-dir lilac &
 pid[0]=$!
 
-# poetry run watchmedo auto-restart \
-#   --patterns="*.py" \
-#   --debounce-interval=1 \
-#   --no-restart-on-command-exit \
-#   --recursive \
-#   --directory=lilac \
-#   poetry -- -- run python -m scripts.make_fastapi_client &
-# pid[0]=$!
+poetry run watchmedo auto-restart \
+  --patterns="*.py" \
+  --debounce-interval=1 \
+  --no-restart-on-command-exit \
+  --recursive \
+  --directory=lilac \
+  poetry -- -- run python -m scripts.make_fastapi_client &
+pid[0]=$!
 
 # When control+c is pressed, kill all process ids.
 trap "pkill -P $$;  exit 1" INT
--- web/blueprint/vite.config.ts
+++ web/blueprint/vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '^/blueprint': {
-        rewrite: path => path.replace('/blueprint', ''),
-        target: 'http://localhost:5173/'
-      },
       '^/api': 'http://127.0.0.1:5432',
       // Listing data files.
       '^/_data': 'http://127.0.0.1:5432',
@@ -24,7 +20,12 @@ export default defineConfig({
       '/load_config': 'http://127.0.0.1:5432',
       // OpenAPI docs
       '^/docs': 'http://127.0.0.1:5432',
-      '/openapi.json': 'http://127.0.0.1:5432'
+      '/openapi.json': 'http://127.0.0.1:5432',
+      // Simulate the app being served under /blueprint base path for testing.
+      '^/blueprint': {
+        rewrite: path => path.replace('/blueprint', ''),
+        target: 'http://localhost:5173/'
+      }
     }
   }
 });
